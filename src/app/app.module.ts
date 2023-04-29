1504f44a94b104581feee00232575e53274e9dbb
Maximiliano <maximilianogatica83@gmail.com>
2023-04-29 20:38:26 -0300

    html y css pasados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './component/header/header.component';
+import { SobreMiComponent } from './component/sobre-mi/sobre-mi.component';
+import { ExperienciaLaboralComponent } from './component/experiencia-laboral/experiencia-laboral.component';
+import { EducacionComponent } from './component/educacion/educacion.component';
+import { HabilidadesComponent } from './component/habilidades/habilidades.component';
+import { ProyectosComponent } from './component/proyectos/proyectos.component';
+import { ContactoComponent } from './component/contacto/contacto.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { InicioComponent } from './component/inicio/inicio.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SobreMiComponent,
+    ExperienciaLaboralComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    ContactoComponent,
+    FooterComponent,
+    InicioComponent,
+  ],
   imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
